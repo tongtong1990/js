7e41ba13c3d0a37b0d89e5b9e4278d36d543ad1e
Jingge Zhou <jizhou@linkedin.com>
2013-07-11 19:43:05 -0700

    22

--- js/conn.js
+++ js/conn.js
@@ -1,5 +1,4 @@
 var socket = io.connect('http://172.19.196.93:9200');
-var left_cnt = 0, right_cnt = 0;
 function go_to_right_screen(tself_id, tright_id, snake_id, head_coord){
 	socket.emit('break_wall_from_left',{self_id:tself_id, target_id:tright_id, snake_id:snake_id, head_location:head_coord});
 }
