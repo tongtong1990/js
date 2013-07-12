c4595d822b70fc53d0cd4b285e7dbf0ea82e2454
Jingge Zhou <jizhou@linkedin.com>
2013-07-11 19:30:34 -0700

    22

--- js/conn.js
+++ js/conn.js
@@ -1,10 +1,10 @@
 			var socket = io.connect('http://172.19.196.93:9200');
 			var left_cnt = 0, right_cnt = 0;
-			function go_to_right_screen(tself_id, tright_id, snake_id){
-				socket.emit('break_wall',{self_id:tself_id, target_id:tright_id, snake_id:snake_id});
+			function go_to_right_screen(tself_id, tright_id, snake_id, head_coord){
+				socket.emit('break_wall_from_left',{self_id:tself_id, target_id:tright_id, snake_id:snake_id, head_location:head_coord});
 			}
-			function go_to_left_screen(tself_id, tleft_id, snake_id){
-				socket.emit('break_wall',{self_id:tself_id, target_id:tleft_id, snake_id:snake_id});
+			function go_to_left_screen(tself_id, tleft_id, snake_id, head_coord){
+				socket.emit('break_wall_from_right',{self_id:tself_id, target_id:tleft_id, snake_id:snake_id, head_location:head_coord});
 			}
 			function eat(snake_id, pic_id){
 				socket.emit('eat',{snake_id:snake_id, pic_id:pic_id});
@@ -17,13 +17,19 @@
 
 				});
 
-				socket.on('new_snake', function(data){
-					var new_snake_id = data.new_snake_id;
 
+				socket.on('new_snake_from_left', function(data){
+					var new_snake_id = data.new_snake_id;
+					var head_location = data.head_location;
 					//to be added : add a new snake in this screen
 					//to be added : set interval
 
 				});
+
+				socket.on('new_snake_from_right', function(data){
+					var new_snake_id = data.new_snake_id;
+					var head_location = data.head_location;
+				})
 
 				socket.on('generate_new_food',function(data){
 					//to be added : choose a position to generate food
