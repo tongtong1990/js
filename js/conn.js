1d2dc46686f776b0705f798349c7cbf452e83df9
Jingge Zhou <jizhou@linkedin.com>
2013-07-11 19:39:22 -0700

    22

--- js/conn.js
+++ js/conn.js
@@ -1,74 +1,11 @@
-			var socket = io.connect('http://172.19.196.93:9200');
-			var left_cnt = 0, right_cnt = 0;
-			function go_to_right_screen(tself_id, tright_id, snake_id, head_coord){
-				socket.emit('break_wall_from_left',{self_id:tself_id, target_id:tright_id, snake_id:snake_id, head_location:head_coord});
-			}
-			function go_to_left_screen(tself_id, tleft_id, snake_id, head_coord){
-				socket.emit('break_wall_from_right',{self_id:tself_id, target_id:tleft_id, snake_id:snake_id, head_location:head_coord});
-			}
-			function eat(snake_id, pic_id){
-				socket.emit('eat',{snake_id:snake_id, pic_id:pic_id});
-			}
-			$(function(){
-				socket.on('header_change',function(data){
-					var changed_snake_id = data.head_device;
-
-					//to be added : change the header
-
-				});
-
-
-				socket.on('new_snake_from_left', function(data){
-					var new_snake_id = data.new_snake_id;
-					var head_location = data.head_location;
-					//to be added : add a new snake in this screen
-					//to be added : set interval
-
-				});
-
-				socket.on('new_snake_from_right', function(data){
-					var new_snake_id = data.new_snake_id;
-					var head_location = data.head_location;
-				})
-
-				socket.on('generate_new_food',function(data){
-					//to be added : choose a position to generate food
-					var new_pic_id = Math.floor(Math.random()*cnt);
-					var pic_obj = document.getElementById(new_pic_id);
-					var pic_src = pic_obj.src;
-					socket.emit('broadcast_food',{pic_id:new_pic_id, pic_src:pic_src});
-				});
-
-				socket.on('snake_eat',function(data){
-					var snake_id = data.snake_id;
-					var pic_id = data.pic_id;
-					//to be added : refresh the status of snake
-				});
-
-				socket.on('new_food_generated',function(data){
-					var pic_id = data.pic_id;
-					var pic_src = data.pic_src;
-					//add ( download ) the photo from the src. add it to hidden area
-					var new_html_code = "<img id=\"" + pic_id +"\" style=\"display:none\" align=\"baseline\" src=\"" + pic_src + "\"></a>";
-					ids[cnt] = pic_id;
-					cnt ++;
-					document.getElementById("connectionsdata").innerHTML += new_html_code;
-				});
-
-				$(document).on('swiperight', function(e){
-					socket.emit('conn_left',{me:tetris.self_id});
-					socket.on('conn_left_confirm', function(data){
-						tetris.self_id = data.self_id;
-						tetris.right_id = data.right_id;
-						alert('Left screen confirm');
-					});
-				});
-				$(document).on('swipeleft', function(e){
-					socket.emit('conn_right',{me:tetris.self_id});
-					socket.on('conn_right_confirm',function(data){
-						tetris.self_id = data.self_id;
-						tetris.left_id = data.left_id;
-						alert('Right screen confirm');
-					});
-				})
-			});
+var socket = io.connect('http://172.19.196.93:9200');
+var left_cnt = 0, right_cnt = 0;
+function go_to_right_screen(tself_id, tright_id, snake_id, head_coord){
+	socket.emit('break_wall_from_left',{self_id:tself_id, target_id:tright_id, snake_id:snake_id, head_location:head_coord});
+}
+function go_to_left_screen(tself_id, tleft_id, snake_id, head_coord){
+	socket.emit('break_wall_from_right',{self_id:tself_id, target_id:tleft_id, snake_id:snake_id, head_location:head_coord});
+}
+function eat(snake_id, pic_id){
+	socket.emit('eat',{snake_id:snake_id, pic_id:pic_id});
+}
